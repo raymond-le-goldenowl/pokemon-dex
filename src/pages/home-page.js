421e2f8e88a2e0584234640dea24eff5351e1f6c
raymond-le-goldenowl <raymond.le.goldenowl@gmail.com>
2021-12-28 16:38:10 +0700

    Styles card pokemon

--- src/pages/home-page.js
+++ src/pages/home-page.js
@@ -22,9 +22,9 @@ export default function HomePage() {
       </form>
 
       {/* List Carts Of Pokemon */}
-      <div className="row">
-        <div className="col-lg-3 col-md-4 mt-4">
-          <div className="card">
+      <div className="row mb-5" id="list-of-pokemon">
+        <div className="col-xl-3 col-lg-4 col-md-6 mt-4">
+          <div className="card card-pokemon">
             <img
               src="https://dummyimage.com/180x120/dbdbdb/787878.png&text=Image+cap"
               className="card-img-top"
@@ -36,15 +36,15 @@ export default function HomePage() {
                 Some quick example text to build on the card title and make up
                 the bulk of the card's content.
               </p>
-              <a href="#!" className="btn btn-primary">
+              <a href="#!" className="btn btn-purple">
                 Go somewhere
               </a>
             </div>
           </div>
         </div>
 
-        <div className="col-lg-3 col-md-4 mt-4">
-          <div className="card">
+        <div className="col-xl-3 col-lg-4 col-md-6 mt-4">
+          <div className="card card-pokemon">
             <img
               src="https://dummyimage.com/180x120/dbdbdb/787878.png&text=Image+cap"
               className="card-img-top"
@@ -56,15 +56,15 @@ export default function HomePage() {
                 Some quick example text to build on the card title and make up
                 the bulk of the card's content.
               </p>
-              <a href="#!" className="btn btn-primary">
+              <a href="#!" className="btn btn-purple">
                 Go somewhere
               </a>
             </div>
           </div>
         </div>
 
-        <div className="col-lg-3 col-md-4 mt-4">
-          <div className="card">
+        <div className="col-xl-3 col-lg-4 col-md-6 mt-4">
+          <div className="card card-pokemon">
             <img
               src="https://dummyimage.com/180x120/dbdbdb/787878.png&text=Image+cap"
               className="card-img-top"
@@ -76,15 +76,15 @@ export default function HomePage() {
                 Some quick example text to build on the card title and make up
                 the bulk of the card's content.
               </p>
-              <a href="#!" className="btn btn-primary">
+              <a href="#!" className="btn btn-purple">
                 Go somewhere
               </a>
             </div>
           </div>
         </div>
 
-        <div className="col-lg-3 col-md-4 mt-4">
-          <div className="card">
+        <div className="col-xl-3 col-lg-4 col-md-6 mt-4">
+          <div className="card card-pokemon">
             <img
               src="https://dummyimage.com/180x120/dbdbdb/787878.png&text=Image+cap"
               className="card-img-top"
@@ -96,15 +96,15 @@ export default function HomePage() {
                 Some quick example text to build on the card title and make up
                 the bulk of the card's content.
               </p>
-              <a href="#!" className="btn btn-primary">
+              <a href="#!" className="btn btn-purple">
                 Go somewhere
               </a>
             </div>
           </div>
         </div>
 
-        <div className="col-lg-3 col-md-4 mt-4">
-          <div className="card">
+        <div className="col-xl-3 col-lg-4 col-md-6 mt-4">
+          <div className="card card-pokemon">
             <img
               src="https://dummyimage.com/180x120/dbdbdb/787878.png&text=Image+cap"
               className="card-img-top"
@@ -116,15 +116,15 @@ export default function HomePage() {
                 Some quick example text to build on the card title and make up
                 the bulk of the card's content.
               </p>
-              <a href="#!" className="btn btn-primary">
+              <a href="#!" className="btn btn-purple">
                 Go somewhere
               </a>
             </div>
           </div>
         </div>
 
-        <div className="col-lg-3 col-md-4 mt-4">
-          <div className="card">
+        <div className="col-xl-3 col-lg-4 col-md-6 mt-4">
+          <div className="card card-pokemon">
             <img
               src="https://dummyimage.com/180x120/dbdbdb/787878.png&text=Image+cap"
               className="card-img-top"
@@ -136,12 +136,50 @@ export default function HomePage() {
                 Some quick example text to build on the card title and make up
                 the bulk of the card's content.
               </p>
-              <a href="#!" className="btn btn-primary">
+              <a href="#!" className="btn btn-purple">
                 Go somewhere
               </a>
             </div>
           </div>
         </div>
+      </div>
+
+      {/* Pagination */}
+      <div className="row" id="pokemon-pagination">
+        <nav aria-label="Page navigation example">
+          <ul className="pagination justify-content-center">
+            <li className="page-item disabled">
+              <a
+                className="page-link"
+                href="#!"
+                tabIndex="-1"
+                aria-disabled="true"
+              >
+                Previous
+              </a>
+            </li>
+            <li className="page-item active">
+              <a className="page-link" href="#!">
+                1
+              </a>
+            </li>
+            <li className="page-item">
+              <a className="page-link" href="#!">
+                2
+              </a>
+            </li>
+            <li className="page-item">
+              <a className="page-link" href="#!">
+                3
+              </a>
+            </li>
+            <li className="page-item">
+              <a className="page-link" href="#!">
+                Next
+              </a>
+            </li>
+          </ul>
+        </nav>
       </div>
     </div>
   )
