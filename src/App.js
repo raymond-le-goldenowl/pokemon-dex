d9896c52887474b187df0d19da709da87a0d2132
raymond-le-goldenowl <raymond.le.goldenowl@gmail.com>
2021-12-28 14:43:00 +0700

    Base ui home page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import HomePage from './pages/home-page'
+
 function App() {
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      <HomePage />
+    </div>
+  )
 }
 
 export default App
---
+++ src/pages/home-page.js
@@ -0,0 +1,147 @@
+import React from 'react'
+
+export default function HomePage() {
+  return (
+    <div className="container-fluid">
+      {/* Logo */}
+      <h1 className="text-center">Pokemon Dex</h1>
+      {/* ! Search Form*/}
+      <form>
+        <div className="mb-3">
+          <label htmlFor="SearchPokemon" className="form-label">
+            Search pokemon
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="SearchPokemon"
+            aria-describedby="SearchPokemon"
+          />
+        </div>
+      </form>
+
+      {/* List Carts Of Pokemon */}
+      <div className="row">
+        <div className="col-lg-3 col-md-4 mt-4">
+          <div className="card">
+            <img
+              src="https://dummyimage.com/180x120/dbdbdb/787878.png&text=Image+cap"
+              className="card-img-top"
+              alt="..."
+            />
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <a href="#!" className="btn btn-primary">
+                Go somewhere
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-lg-3 col-md-4 mt-4">
+          <div className="card">
+            <img
+              src="https://dummyimage.com/180x120/dbdbdb/787878.png&text=Image+cap"
+              className="card-img-top"
+              alt="..."
+            />
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <a href="#!" className="btn btn-primary">
+                Go somewhere
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-lg-3 col-md-4 mt-4">
+          <div className="card">
+            <img
+              src="https://dummyimage.com/180x120/dbdbdb/787878.png&text=Image+cap"
+              className="card-img-top"
+              alt="..."
+            />
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <a href="#!" className="btn btn-primary">
+                Go somewhere
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-lg-3 col-md-4 mt-4">
+          <div className="card">
+            <img
+              src="https://dummyimage.com/180x120/dbdbdb/787878.png&text=Image+cap"
+              className="card-img-top"
+              alt="..."
+            />
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <a href="#!" className="btn btn-primary">
+                Go somewhere
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-lg-3 col-md-4 mt-4">
+          <div className="card">
+            <img
+              src="https://dummyimage.com/180x120/dbdbdb/787878.png&text=Image+cap"
+              className="card-img-top"
+              alt="..."
+            />
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <a href="#!" className="btn btn-primary">
+                Go somewhere
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-lg-3 col-md-4 mt-4">
+          <div className="card">
+            <img
+              src="https://dummyimage.com/180x120/dbdbdb/787878.png&text=Image+cap"
+              className="card-img-top"
+              alt="..."
+            />
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <a href="#!" className="btn btn-primary">
+                Go somewhere
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
