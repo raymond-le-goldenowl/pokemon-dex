f94e45a3fb1ab2c6cec38f7a89d634f0c9d76095
raymond-le-goldenowl <raymond.le.goldenowl@gmail.com>
2021-12-31 18:25:02 +0700

    update page detail: display evolution

--- src/pages/detail-page.js
+++ src/pages/detail-page.js
@@ -18,13 +18,39 @@ export default function DetailPage() {
     weight: '',
     height: '',
     abilities: [],
-    stats: []
+    stats: [],
+    url_species: ''
   }))
+
+  const [evolutionChain, setEvolutionChain] = useState(() => ({}))
 
   // get detail of pokemon.
   const fetchDetailPokemon = useCallback(url => {
     return axios.get(url).then(response => response.data)
   }, [])
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect(async () => {
+    const species = await axios.get(`${detailPokemon.url_species}`)
+    const _evolutionChain = await axios.get(
+      `${species.data.evolution_chain?.url}`
+    )
+    const data = _evolutionChain.data?.chain
+    const species1 = data.species.name
+    const species1_url = data.species.url
+
+    const species2 = data.evolves_to[0]?.species.name
+    const species2_url = data.evolves_to[0]?.species.url
+
+    const species3 = data.evolves_to[0]?.evolves_to[0]?.species.name
+    const species3_url = data.evolves_to[0]?.evolves_to[0]?.species.url
+
+    setEvolutionChain(() => [
+      { name: species1, url: species1_url },
+      { name: species2, url: species2_url },
+      { name: species3, url: species3_url }
+    ])
+  }, [detailPokemon])
 
   useEffect(() => {
     fetchDetailPokemon(`https://pokeapi.co/api/v2/pokemon/${id}/`).then(
@@ -42,7 +68,7 @@ export default function DetailPage() {
         data.height = response?.height
         data.abilities = response?.abilities
         data.stats = response?.stats
-        console.log(data)
+        data.url_species = response?.species.url
 
         // set data for state.
         setDetailPokemon(data)
@@ -129,9 +155,15 @@ export default function DetailPage() {
         <div className="col-md-6">
           <h3 className="title">Evolution</h3>
           <div className="row pts-content-sm">
-            <div className="col-4">hihi</div>
-            <div className="col-4">hihi</div>
-            <div className="col-4">hihi</div>
+            {Object.values(evolutionChain).map((chain, index) => {
+              return (
+                <Variety
+                  key={index + Date.now()}
+                  pokemon={chain}
+                  index={index}
+                />
+              )
+            })}
           </div>
         </div>
       </div>
@@ -147,11 +179,9 @@ const EffectItem = url => {
   const [effect, setEffect] = useState('')
 
   useEffect(() => {
-    console.log()
     axios.get(`${url.url}`).then(response => {
       // eslint-disable-next-line array-callback-return
       Object.values(response.data?.effect_entries).map(effect_entry => {
-        // console.log(effect_entry.language)
         if (effect_entry?.language?.name === 'en') {
           setEffect(effect_entry.effect)
         }
@@ -160,4 +190,52 @@ const EffectItem = url => {
   }, [url])
 
   return <li>{effect}</li>
+}
+
+const Variety = ({ pokemon, index }) => {
+  // save state detail pokemon.
+  const [detailPokemon, setDetailPokemon] = useState(() => ({
+    id: '',
+    name: '',
+    front_default: ''
+  }))
+
+  // get detail of pokemon.
+  const fetchDetailPokemon = useCallback(url => {
+    const _url = `${url}`.replaceAll('-species', '')
+    return axios.get(_url).then(response => response.data)
+  }, [])
+
+  useEffect(() => {
+    fetchDetailPokemon(`${pokemon?.url}`).then(response => {
+      // assignment data.
+      let data = {}
+      data.id = response?.id
+      data.name = response?.name
+      data.front_default = response?.sprites?.front_default
+
+      // set data for state.
+      setDetailPokemon(data)
+    })
+  }, [pokemon, fetchDetailPokemon])
+
+  return (
+    <div className="col-4 text-center">
+      <div>#{index + 1}</div>
+      <Link to={`/detail/${detailPokemon.id}`}>
+        <img
+          src={`${detailPokemon.front_default}`}
+          alt={`${detailPokemon.name}`}
+        />
+      </Link>
+
+      <Link
+        className="d-block h5"
+        style={{ textDecoration: 'none', color: 'black' }}
+        to={`/detail/${detailPokemon.id}`}
+      >
+        {detailPokemon.name}
+      </Link>
+    </div>
+  )
 }
