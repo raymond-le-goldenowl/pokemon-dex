aa699ec8260741ff790f917e37cdba8aeb76973b
Raymond <96218923+raymond-le-goldenowl@users.noreply.github.com>
2022-01-04 12:41:43 +0700

    Feature/homepage paginate pokemons (#8)
    * Display list of pokemons
    
    * Paginate pokemons
    
    * Update dynamic paginate

--- src/pages/home-page.js
+++ src/pages/home-page.js
@@ -1,20 +1,50 @@
 import axios from 'axios'
-import React, { useCallback, useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useRef, useState } from 'react'
+import useWindowPosition from '../hooks/useWindowPosition'
 import './home.css'
 
 export default function HomePage() {
-  const [pokemonsPaginateData, setPokemonsPaginateData] = useState([])
+  // save all pokemons.
+  const pokemonsRef = useRef()
+  const [pokemons, setPokemons] = useState([])
+  // Pokemon for display to screen.
+  const [pokemonsDisplay, setPokemonsDisplay] = useState([])
 
-  const fetchPokemonsPaginateData = useCallback((offset = 0, limit = 20) => {
-    const url = `https://pokeapi.co/api/v2/pokemon?${offset}=1&limit=${limit}`
+  const limit = 1118
+  const [offset, setOffset] = useState(0)
+  // Hook for get scroll position.
+  const pageScrollData = useWindowPosition()
+  const divListPokemonRef = useRef()
+
+  // Fetch pokemons with params are offset and limit.
+  const fetchPokemons = useCallback((offset = 0, limit = 20) => {
+    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
     return axios.get(url).then(response => response.data)
   }, [])
 
+  // Running when scroll, to check is end of data. For call new data.
   useEffect(() => {
-    fetchPokemonsPaginateData().then(pokemonsPaginateData => {
-      setPokemonsPaginateData(pokemonsPaginateData?.results)
+    const contentHeight = divListPokemonRef.current.offsetHeight
+    const y = pageScrollData + window.innerHeight
+
+    if (y > contentHeight && pageScrollData > 0 && contentHeight > 0) {
+      setOffset(prev => prev + 20)
+    }
+  }, [pageScrollData])
+
+  // Fetch all pokemons fist load.
+  useEffect(() => {
+    fetchPokemons(0, limit).then(resPokemons => {
+      setPokemons(resPokemons?.results)
+      pokemonsRef.current = resPokemons?.results
     })
-  }, [fetchPokemonsPaginateData])
+  }, [fetchPokemons])
+
+  // Slice pokemons when offset changed.
+  useEffect(() => {
+    const pokemonSlice = pokemons.slice(offset, offset + 20)
+    setPokemonsDisplay(prePokemons => [...prePokemons, ...pokemonSlice])
+  }, [pokemons, offset])
 
   return (
     <div className="container">
@@ -36,57 +66,20 @@ export default function HomePage() {
       </form>
 
       {/* List Carts Of Pokemon */}
-      <div className="row mb-5" id="list-of-pokemon">
-        {pokemonsPaginateData.map((pokemon, index) => (
+      <div className="row mb-5" id="list-of-pokemon" ref={divListPokemonRef}>
+        {pokemonsDisplay.map((pokemon, index) => (
           <CardPokemon
-            key={index + Date.now()}
+            key={index}
             pokemon={pokemon}
-            fetchPokemonsPaginateData={fetchPokemonsPaginateData}
+            fetchPokemonsPaginateData={fetchPokemons}
           />
         ))}
-      </div>
-
-      {/* Pagination */}
-      <div className="row" id="pokemon-pagination">
-        <nav aria-label="Page navigation example">
-          <ul className="pagination justify-content-center">
-            <li className="page-item disabled">
-              <a
-                className="page-link"
-                href="#!"
-                tabIndex="-1"
-                aria-disabled="true"
-              >
-                Previous
-              </a>
-            </li>
-            <li className="page-item active">
-              <a className="page-link" href="#!">
-                1
-              </a>
-            </li>
-            <li className="page-item">
-              <a className="page-link" href="#!">
-                2
-              </a>
-            </li>
-            <li className="page-item">
-              <a className="page-link" href="#!">
-                3
-              </a>
-            </li>
-            <li className="page-item">
-              <a className="page-link" href="#!">
-                Next
-              </a>
-            </li>
-          </ul>
-        </nav>
       </div>
     </div>
   )
 }
 
+// Sub component for homepage render Pokemon Card.
 function CardPokemon({ pokemon, fetchPokemonsPaginateData }) {
   // save state detail pokemon.
   const [detailPokemon, setDetailPokemon] = useState(() => ({
